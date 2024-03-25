ae4f3cdd015d5531765e61b78b78502917c47320
rclatot <romainclatotpro.proton.me>
2024-03-25 13:51:12 +0100

    Tryyyy

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const DOMAIN = "zone01normandie.org"; // Domaine de l'API GraphQL
-let token; // Variable pour stocker le jeton d'authentification
-
-// Requête GraphQL pour récupérer les transactions d'XP
+const DOMAIN = "zone01normandie.org"
+let token
 const query = `
   query {
     transaction {
@@ -12,8 +10,6 @@ const query = `
     }
   }
 `;
-
-// Requête GraphQL pour récupérer les informations de l'utilisateur
 const query2 = `
   query {
     user {
@@ -22,85 +18,89 @@ const query2 = `
     }
   }
 `;
-
-// Types de compétences possibles
 const skillTypes = [
-  "algo", "prog", "game", "ai", "stats", "tcp", "unix", "go", "js", "rust", "c", "python", "php", "ruby", "sql", "html", "css", "docker", "back-end", "front-end", "sys-admin"
+  "algo",
+  "prog",
+  "game",
+  "ai",
+  "stats",
+  "tcp",
+  "unix",
+  "go",
+  "js",
+  "rust",
+  "c",
+  "python",
+  "php",
+  "ruby",
+  "sql",
+  "html",
+  "css",
+  "docker",
+  "back-end",
+  "front-end",
+  "sys-admin"
 ];
-
-// Fonction asynchrone pour se déconnecter
 async function logout() {
-  // Cacher le contenu et le bouton de déconnexion
-  document.querySelector('.content-wrapper').style.display = "none";
-  document.querySelector('.logout').style.display = "none";
-  document.querySelector('.personal-info').style.display = "none";
-
-  document.querySelector('.login').style.display = "block";
-
-  // Supprimer le graphique
+  document.querySelector('.content-wrapper').style.display = "none"
+  document.querySelector('.logout').style.display = "none"
+  document.querySelector('.login').style.display = "block"
+  // Remove the chart
   const xpContainer = document.getElementById('xp-container');
   while (xpContainer.firstChild) {
     xpContainer.removeChild(xpContainer.firstChild);
   }
-
   const levelContainer = document.getElementById('level-container');
   while (levelContainer.firstChild) {
     levelContainer.removeChild(levelContainer.firstChild);
   }
 }
-
-// Fonction asynchrone pour se connecter
 async function login() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
   const credentials = btoa(`${username}:${password}`);
-
   try {
-    // Effectuer une demande d'authentification
-    const response = await fetch(`https://zone01normandie.org/api/auth/signin`, {
+    const response = await fetch(`https://${DOMAIN}/api/auth/signin`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Basic ${credentials}`
       },
     });
-
-    // Vérifier si la demande a réussi
     if (!response.ok) {
       throw new Error('Invalid credentials');
     }
-
-    // Récupérer le jeton d'authentification
     token = await response.json();
+    document.querySelector('.logout').style.display = "flex"
+    document.querySelector('.content-wrapper').style.display = "flex"
+    document.querySelector('.login').style.display = "none"
+    document.getElementById('error').innerText = ""
+    const user = await getDataUser()
+    const data = await getDataXP()
+    console.log(user)
+    const test = createGraphXP(data)
+    const ratio = createRatio(data)
+    console.log(ratio)
+    const level = createLevel(data)
+    console.log(level)
+    document.getElementById('resultat').innerText = `TOTAL XP : ${test}`;
+    document.getElementById('top-xp').innerText = `${test}`;
+    document.getElementById('top-level').innerText = `100`;
+    document.getElementById('id').innerText = `ID : ${user.id}`;
+    document.getElementById('ratio').innerText = `Ratio : ${ratio}`;
+    document.getElementById('level').innerText = `Level : ${level}`;
+    document.getElementById('welcome').innerText = `Welcome ${user.attrs.firstName} ${user.attrs.lastName}`;
 
-    // Afficher les éléments appropriés après la connexion réussie
-    document.querySelector('.logout').style.display = "block";
-    document.querySelector('.personal-info').style.display = "block";
-    document.querySelector('.login').style.display = "none";
-    document.getElementById('error').innerText = "";
-
-    // Récupérer les données d'utilisateur et d'XP
-    const user = await getDataUser();
-    const data = await getDataXP();
-
-   // Créer le graphique d'XP, le ratio et le niveau
-    createSkillBarGraph(data);
-    const ratio = createRatio(data);
-    const level = createLevel(data);
-
-    // Afficher les données récupérées dans la console
-    console.log(user);
-    console.log(data);
-    console.log(ratio);
-    console.log(level);
-
+    console.log("Skill Levels:");
+    const skillLevels = createSkills(data, skillTypes);
+    Object.entries(skillLevels).forEach(([skillType, level]) => {
+      console.log(`${skillType}: ${level}`);
+    });
+    createSkillBarGraph(skillLevels);
   } catch (error) {
-    // Afficher une erreur en cas d'échec de l'authentification
     document.getElementById('error').innerText = 'Invalid credentials. Please try again.';
   }
 }
-
-// Fonction pour récupérer les données d'XP depuis l'API GraphQL
 async function getDataXP() {
   try {
     const response = await fetch(`https://${DOMAIN}/api/graphql-engine/v1/graphql`, {
@@ -111,25 +111,98 @@ async function getDataXP() {
       },
       body: JSON.stringify({ query }),
     });
-
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
     const data = await response.json();
-
     if (data.errors) {
       throw new Error(`GraphQL error: ${JSON.stringify(data.errors)}`);
     }
-
     return data.data.transaction;
   } catch (error) {
     console.error(error);
     throw new Error('Failed to fetch data');
   }
 }
-
-// Fonction pour récupérer les données de l'utilisateur depuis l'API GraphQL
+function createGraphXP(transactions) {
+  const filteredTransactions = transactions.filter(transaction => {
+    return transaction.path.includes("/div-01") && !transaction.path.includes("piscine-js/");
+  });
+  const data = filteredTransactions.filter(filteredTransaction => {
+    return filteredTransaction.type === "xp";
+  });
+  const sortedData = data.slice().sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+  // Create the SVG container
+  const svgContainer = document.getElementById('xp-container');
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '100%');
+  svg.setAttribute('height', '100%');
+  // Create the line
+  const accumulatedValues = [];
+  let accumulatedTotal = 0;
+  sortedData.forEach((entry, index) => {
+    accumulatedTotal += entry.amount;
+    accumulatedValues.push({ x: index, y: accumulatedTotal });
+  });
+  // Calculate Y-axis step size
+  const yAxisStep = Math.ceil(Math.max(accumulatedTotal));
+  // Calculate X-axis step size (assuming 30 days per month)
+  const dateStep = 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
+  // Calculate the number of months between the start and end date
+  const startDate = new Date(sortedData[0].createdAt);
+  const endDate = new Date(sortedData[sortedData.length - 1].createdAt);
+  const monthsDifference = monthsBetweenDates(startDate, endDate);
+  // Create the line
+  const line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+  const points = accumulatedValues.map(entry => `${(entry.x / (sortedData.length - 1)) * svgContainer.clientWidth},${svgContainer.clientHeight - (entry.y / yAxisStep) * svgContainer.clientHeight}`);
+  line.setAttribute('points', points.join(' '));
+  line.setAttribute('fill', 'none');
+  line.setAttribute('stroke', 'steelblue');
+  line.setAttribute('stroke-width', '2');
+  svg.appendChild(line);
+  // Create Y-axis ticks and labels
+  for (let i = 0; i <= 10; i++) {
+    const yAxisTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    yAxisTick.setAttribute('x1', '0');
+    yAxisTick.setAttribute('x2', svgContainer.clientWidth);
+    yAxisTick.setAttribute('y1', (i / 10) * svgContainer.clientHeight);
+    yAxisTick.setAttribute('y2', (i / 10) * svgContainer.clientHeight);
+    yAxisTick.setAttribute('stroke', '#ccc');
+    svg.appendChild(yAxisTick);
+    const yAxisLabel = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    yAxisLabel.setAttribute('x', '5');
+    yAxisLabel.setAttribute('y', (i / 10) * svgContainer.clientHeight - 5);
+    yAxisLabel.setAttribute('fill', '#333');
+    yAxisLabel.textContent = Math.round(yAxisStep*(10-i)/10);
+    svg.appendChild(yAxisLabel);
+  }
+  // Create X-axis ticks and labels
+  for (let i = 0; i <= monthsDifference; i++) {
+    const dateForTick = new Date(startDate.getTime() + (i / monthsDifference) * monthsDifference * dateStep);
+    const xAxisTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    xAxisTick.setAttribute('x1', (i / monthsDifference) * svgContainer.clientWidth);
+    xAxisTick.setAttribute('x2', (i / monthsDifference) * svgContainer.clientWidth);
+    xAxisTick.setAttribute('y1', '0');
+    xAxisTick.setAttribute('y2', svgContainer.clientHeight);
+    xAxisTick.setAttribute('stroke', '#ccc');
+    svg.appendChild(xAxisTick);
+    const xAxisLabel = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    xAxisLabel.setAttribute('x', (i / monthsDifference) * svgContainer.clientWidth);
+    xAxisLabel.setAttribute('y', svgContainer.clientHeight - 5);
+    xAxisLabel.setAttribute('fill', '#333');
+    xAxisLabel.textContent = formatDate(dateForTick);
+    svg.appendChild(xAxisLabel);
+  }
+  svgContainer.appendChild(svg);
+  return accumulatedTotal;
+}
+function formatDate(date) {
+  const options = { month: 'short', year: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+function monthsBetweenDates(startDate, endDate) {
+  return (endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth() +1;
+}
 async function getDataUser() {
   try {
     const response = await fetch(`https://${DOMAIN}/api/graphql-engine/v1/graphql`, {
@@ -140,106 +213,71 @@ async function getDataUser() {
       },
       body: JSON.stringify({ query: query2 }),
     });
-
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
     const data = await response.json();
-
     if (data.errors) {
       throw new Error(`GraphQL error: ${JSON.stringify(data.errors)}`);
     }
-
     return data.data.user[0];
   } catch (error) {
     console.error(error);
     throw new Error('Failed to fetch data');
   }
 }
-
-// Fonction pour calculer le ratio entre XP gagné et perdu
-function createRatio(transactions) {
+function createRatio(transactions){
   let totalXpDown = 0;
   let totalXpUp = 0;
-
-  // Filtrer les transactions pour récupérer les XP gagnés et perdus
   const xpDown = transactions.filter(transaction => {
     return transaction.type === "down";
   });
-
   const xpUp = transactions.filter(transaction => {
     return transaction.type === "up";
   });
-
-  // Calculer le total des XP gagnés et perdus
-  xpDown.forEach(entry => {
+  xpDown.forEach((entry, index) => {
     totalXpDown += entry.amount;
   });
-
-  xpUp.forEach(entry => {
+  xpUp.forEach((entry, index) => {
     totalXpUp += entry.amount;
   });
-
-  // Calculer et renvoyer le ratio
-  if (totalXpUp !== 0 && totalXpDown !== 0) {
+  if (totalXpUp != 0 && totalXpDown != 0) {
     return (totalXpUp / totalXpDown).toFixed(3);
   } else {
     return 0;
   }
 }
-
-// Fonction pour récupérer le niveau maximum atteint par l'utilisateur
-function createLevel(transactions) {
+function createLevel(transactions){
   let level = 0;
-
-  // Filtrer les transactions pour ne garder que celles liées au niveau
   const filteredTransactions = transactions.filter(transaction => {
     return transaction.path.includes("/div-01") && !transaction.path.includes("piscine-js/");
   });
-
   const filterLevelTransactions = filteredTransactions.filter(transaction => {
     return transaction.type === "level";
   });
-
-  // Trouver le niveau le plus élevé parmi les transactions filtrées
-  filterLevelTransactions.forEach(entry => {
+  filterLevelTransactions.forEach((entry, index) => {
     if (entry.amount > level) {
       level = entry.amount;
     }
   });
-
   return level;
 }
-
 function createSkills(transactions, skillTypes) {
   const skillLevels = {};
-
-  // Pour chaque type de compétence, trouver le niveau correspondant
   skillTypes.forEach(skillType => {
     let skillLevel = 0;
-
-    // Filtrer les transactions pour ne garder que celles liées à ce type de compétence
     const filterSkillTransactions = transactions.filter(transaction => {
       return transaction.type === `skill_${skillType}`;
     });
-
-    // Trouver le niveau le plus élevé parmi les transactions filtrées
     filterSkillTransactions.forEach(entry => {
       if (entry.amount > skillLevel) {
         skillLevel = entry.amount;
       }
     });
-
-    // Stocker le niveau de compétence pour ce type de compétence
     skillLevels[skillType] = skillLevel;
   });
-
   return skillLevels;
 }
-
-// Fonction pour créer un graphique à barres des compétences
-
 async function createSkillBarGraph(skillLevels) {
   const svgContainer = document.getElementById('level-container');
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
