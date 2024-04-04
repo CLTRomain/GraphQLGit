3d53e9e0a59803060e2ca6b3b4c9f111701bc4ef
rclatot <romainclatotpro.proton.me>
2024-04-04 15:10:15 +0200

    Update

--- index.js
+++ index.js
@@ -1,123 +1,35 @@
 const DOMAIN = "zone01normandie.org"
 let token
-const query = `
-  query {
-    transaction {
-      type
-      amount
-      path
-      createdAt
-    }
-  }
-`;
-const query2 = `
+const userQuery = `
   query {
     user {
       id
       attrs
+      transactions {
+        type
+        amount
+        path
+        createdAt
+      }
     }
   }
 `;
-const skillTypes = [
-  "algo",
-  "prog",
-  "game",
-  "ai",
-  "stats",
-  "tcp",
-  "unix",
-  "go",
-  "js",
-  "rust",
-  "c",
-  "python",
-  "php",
-  "ruby",
-  "sql",
-  "html",
-  "css",
-  "docker",
-  "back-end",
-  "front-end",
-  "sys-admin"
-];
-async function logout() {
-  document.querySelector('.content-wrapper').style.display = "none"
-  document.querySelector('.logout').style.display = "none"
-  document.querySelector('.login').style.display = "block"
-  // Remove the chart
-  const xpContainer = document.getElementById('xp-container');
-  while (xpContainer.firstChild) {
-    xpContainer.removeChild(xpContainer.firstChild);
-  }
-  const levelContainer = document.getElementById('level-container');
-  while (levelContainer.firstChild) {
-    levelContainer.removeChild(levelContainer.firstChild);
-  }
+
+const skillTypesQuery = `
+  query{
+  transaction(where: {
+              type: {_ilike: "skill_%"}
+              },
+          ) { 
+  type,
+  amount,
+  createdAt,
+  path,
+},
 }
-async function login() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const credentials = btoa(`${username}:${password}`);
-  try {
-    const response = await fetch(`https://${DOMAIN}/api/auth/signin`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${credentials}`
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Invalid credentials');
-    }
-    token = await response.json();
-    console.log(token)
-    document.querySelector('.logout').style.display = "flex"
-    document.querySelector('.content-wrapper').style.display = "flex"
-    document.querySelector('.login').style.display = "none"
-    document.getElementById('error').innerText = ""
+`;
 
-
-    const user = await getDataUser()
-
-    const data = await getDataXP()
-
-
-    console.log(user)
-
-    const test = createGraphXP(data)
-    const ratio = createRatio(data)
-
-
-    console.log(ratio)
-
-    const level = createLevel(data)
-
-    console.log(level)
-
-    document.getElementById('resultat').innerText = `TOTAL XP : ${test}`;
-    document.getElementById('top-xp').innerText = `${test}`;
-    document.getElementById('id').innerText = `ID : ${user.id}`;
-    document.getElementById('ratio').innerText = `Ratio : ${ratio}`;
-    document.getElementById('level').innerText = `Level : ${level}`;
-    document.getElementById('welcome').innerText = `Welcome ${user.attrs.firstName} ${user.attrs.lastName}`;
-
-    console.log("Skill Levels:");
-    const skillLevels = createSkills(data, skillTypes);
-    console.log(skillLevels)
-    Object.entries(skillLevels).forEach(([skillType, level]) => {
-      console.log(`${skillType}: ${level}`);
-    });
-
-
-    createSkillBarGraph(skillLevels);
-
-
-  } catch (error) {
-    document.getElementById('error').innerText = 'Invalid credentials. Please try again.';
-  }
-}
-async function getDataXP() {
+async function getDataSkill() {
   try {
     const response = await fetch(`https://${DOMAIN}/api/graphql-engine/v1/graphql`, {
       method: 'POST',
@@ -125,7 +37,7 @@ async function getDataXP() {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       },
-      body: JSON.stringify({ query }),
+      body: JSON.stringify({ query : skillTypesQuery}),
     });
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -142,6 +54,115 @@ async function getDataXP() {
 }
 
 
+async function logout() {
+  document.querySelector('.content-wrapper').style.display = "none";
+  document.querySelector('.logout').style.display = "none";
+  document.querySelector('.login').style.display = "block";
+
+  const xpContainer = document.getElementById('xp-container');
+  while (xpContainer.firstChild) {
+    xpContainer.removeChild(xpContainer.firstChild);
+  }
+}
+async function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const credentials = btoa(`${username}:${password}`);
+
+  try {
+    const response = await fetch(`https://${DOMAIN}/api/auth/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${credentials}`
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Invalid credentials');
+    }
+
+    token = await response.json();
+
+    document.querySelector('.logout').style.display = "flex";
+    document.querySelector('.content-wrapper').style.display = "flex";
+    document.querySelector('.login').style.display = "none";
+    document.getElementById('error').innerText = "";
+
+    const user = await getDataUser();
+    const data = await getDataXP();
+    const skill = await getDataSkill();
+
+    console.log(user);
+    console.log(data);
+    console.log(skill);
+
+    const test = createGraphXP(data);
+    const ratio = createRatio(data);
+    const level = createLevel(data);
+    console.log("test")
+
+    document.getElementById('resultat').innerText = `XP : ${test}`;
+    document.getElementById('id').innerText = `ID : ${user.id}`;
+
+    document.getElementById('ratio').innerText = `Ratio : ${ratio}`;
+    console.log("test1")
+
+    document.getElementById('level').innerText = `Level : ${level}`;
+    console.log("test2")
+
+    try {
+      document.getElementById('name').innerText = `${user.attrs.firstName} ${user.attrs.lastName}`;
+      console.log("After setting name innerText");
+    } catch (error) {
+      console.error("Error setting name innerText:", error);
+    }    console.log("test3")
+
+    document.getElementById('welcome').innerText = `Welcome, ${user.attrs.firstName} ${user.attrs.lastName}`;
+    console.log("test4")
+
+
+    console.log("Skill Levels:");
+    const skillLevels = createSkills(skill); // Utilisez createSkills avec les données de compétence
+    console.log(skillLevels);
+    Object.entries(skillLevels).forEach(([skillType, level]) => {
+      console.log(`${skillType}: ${level}`);
+    });
+
+     await createSkillBarGraph(skillLevels);
+  } catch (error) {
+    document.getElementById('error').innerText = 'Invalid credentials. Please try again.';
+  }
+}
+async function getDataXP() {
+  try {
+    const response = await fetch(`https://${DOMAIN}/api/graphql-engine/v1/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({ query: userQuery }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.errors) {
+      throw new Error(`GraphQL error: ${JSON.stringify(data.errors)}`);
+    }
+
+    return data.data.user[0].transactions;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to fetch data');
+  }
+}
+
+
 
 
 function createGraphXP(transactions) {
@@ -149,7 +170,6 @@ function createGraphXP(transactions) {
   const filteredTransactions = transactions.filter(transaction => {
     return transaction.path.includes("/div-01") && !transaction.path.includes("piscine-js/");
   });
-
 
   // Filtrer les transactions pour inclure uniquement celles de type "xp"
   const data = filteredTransactions.filter(filteredTransaction => {
@@ -159,15 +179,13 @@ function createGraphXP(transactions) {
   // Trier les données par ordre chronologique
   const sortedData = data.slice().sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
 
-
   // Créer le conteneur SVG
   const svgContainer = document.getElementById('xp-container');
-
 
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('width', '100%');
   svg.setAttribute('height', '100%');
-
+  svg.style.backgroundColor = 'lightgray'; // Changed background color
 
 
   // Créer la ligne du graphique
@@ -176,22 +194,21 @@ function createGraphXP(transactions) {
 
   sortedData.forEach((entry, index) => {
     accumulatedTotal += entry.amount;
-    console.log(accumulatedTotal)
     accumulatedValues.push({ x: index, y: accumulatedTotal });
   });
 
-  // Calculer la taille des graduations de l'axe Y
-  const yAxisStep = Math.ceil(Math.max(accumulatedTotal));
+  // Calculer les coordonnées des points pour remplir la zone sous la ligne du graphique
+  const fillPoints = accumulatedValues.map(entry => `${(entry.x / (sortedData.length - 1)) * svgContainer.clientWidth},${svgContainer.clientHeight - (entry.y / accumulatedTotal) * svgContainer.clientHeight}`).join(' ');
 
-  // Calculer la taille des graduations de l'axe X
-  const dateStep = 30 * 24 * 60 * 60 * 1000; // 30 jours en millisecondes
-  const startDate = new Date(sortedData[0].createdAt);
-  const endDate = new Date(sortedData[sortedData.length - 1].createdAt);
-  const monthsDifference = monthsBetweenDates(startDate, endDate);
+  // Créer le polygone pour remplir la zone sous la ligne du graphique
+  const fillArea = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+  fillArea.setAttribute('points', `0,${svgContainer.clientHeight} ${fillPoints} ${svgContainer.clientWidth},${svgContainer.clientHeight}`);
+  fillArea.setAttribute('fill', 'steelblue'); // Couleur de remplissage bleue
+  svg.appendChild(fillArea);
 
   // Créer la ligne du graphique
   const line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-  const points = accumulatedValues.map(entry => `${(entry.x / (sortedData.length - 1)) * svgContainer.clientWidth},${svgContainer.clientHeight - (entry.y / yAxisStep) * svgContainer.clientHeight}`);
+  const points = accumulatedValues.map(entry => `${(entry.x / (sortedData.length - 1)) * svgContainer.clientWidth},${svgContainer.clientHeight - (entry.y / accumulatedTotal) * svgContainer.clientHeight}`);
   line.setAttribute('points', points.join(' '));
   line.setAttribute('fill', 'none');
   line.setAttribute('stroke', 'steelblue');
@@ -212,13 +229,16 @@ function createGraphXP(transactions) {
     yAxisLabel.setAttribute('x', '5');
     yAxisLabel.setAttribute('y', (i / 10) * svgContainer.clientHeight - 5);
     yAxisLabel.setAttribute('fill', '#333');
-    yAxisLabel.textContent = Math.round(yAxisStep*(10-i)/10);
+    yAxisLabel.textContent = Math.round(accumulatedTotal * (1 - i / 10)); // Calculer les valeurs affichées sur l'axe Y
     svg.appendChild(yAxisLabel);
   }
 
   // Créer les graduations de l'axe X
+  const startDate = new Date(sortedData[0].createdAt);
+  const endDate = new Date(sortedData[sortedData.length - 1].createdAt);
+  const monthsDifference = monthsBetweenDates(startDate, endDate);
   for (let i = 0; i <= monthsDifference; i++) {
-    const dateForTick = new Date(startDate.getTime() + (i / monthsDifference) * monthsDifference * dateStep);
+    const dateForTick = new Date(startDate.getTime() + (i / monthsDifference) * (endDate.getTime() - startDate.getTime()));
     const xAxisTick = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     xAxisTick.setAttribute('x1', (i / monthsDifference) * svgContainer.clientWidth);
     xAxisTick.setAttribute('x2', (i / monthsDifference) * svgContainer.clientWidth);
@@ -231,16 +251,18 @@ function createGraphXP(transactions) {
     xAxisLabel.setAttribute('x', (i / monthsDifference) * svgContainer.clientWidth);
     xAxisLabel.setAttribute('y', svgContainer.clientHeight - 5);
     xAxisLabel.setAttribute('fill', '#333');
-    xAxisLabel.textContent = formatDate(dateForTick);
+    xAxisLabel.textContent = formatDate(dateForTick); // Afficher les dates sur l'axe X
     svg.appendChild(xAxisLabel);
   }
 
   // Ajouter le graphique au conteneur SVG
   svgContainer.appendChild(svg);
 
-  // Retourner la somme totale accumulée (non utilisée dans ce code)
+  // Retourner la somme totale accumulée
   return accumulatedTotal;
 }
+
+
 
 function formatDate(date) {
   const options = { month: 'short', year: 'numeric' };
@@ -257,19 +279,23 @@ async function getDataUser() {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       },
-      body: JSON.stringify({ query: query2 }),
+      body: JSON.stringify({ query: userQuery }),
     });
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
     const data = await response.json();
+
     if (data.errors) {
       throw new Error(`GraphQL error: ${JSON.stringify(data.errors)}`);
     }
+
     return data.data.user[0];
   } catch (error) {
     console.error(error);
-    throw new Error('Failed to fetch data');
+    throw new Error('Failed to fetch user data');
   }
 }
 function createRatio(transactions){
@@ -308,29 +334,27 @@ function createLevel(transactions){
   });
   return level;
 }
-function createSkills(transactions, skillTypes) {
+function createSkills(transactions) {
   const skillLevels = {};
-
-  // Initialize all skill levels to 0
-  skillTypes.forEach(skillType => {
-    skillLevels[skillType] = 0;
-  });
-
-  // Update skill levels based on transaction data
-  transactions.forEach(transaction => {
-    if (transaction.type.startsWith('skill_')) {
-      const skillType = transaction.type.substring('skill_'.length);
-      if (skillLevels.hasOwnProperty(skillType)) {
-        skillLevels[skillType] = Math.max(skillLevels[skillType], transaction.amount);
+  transactions.forEach((transaction) => {
+    const { type, amount } = transaction;
+    if (type.startsWith("skill_")) {
+      const skillType = type.replace("skill_", "");
+      if (!(skillType in skillLevels) || amount > skillLevels[skillType]) {
+        skillLevels[skillType] = amount;
       }
     }
   });
-
   return skillLevels;
 }
 
 async function createSkillBarGraph(skillLevels) {
+  console.log("Creating skill bar graph...");
+  console.log("Skill levels:", skillLevels);
+
   const svgContainer = document.getElementById('level-container');
+  console.log("SVG Container:", svgContainer);
+
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('width', '100%');
   svg.setAttribute('height', '500'); // Augmentez la hauteur à 500 pixels par exemple
@@ -344,9 +368,11 @@ async function createSkillBarGraph(skillLevels) {
 
   // Get all skill types
   const allSkills = Object.keys(skillLevels);
+  console.log("All skills:", allSkills);
 
   // Sort the skills by level in descending order
   allSkills.sort((a, b) => skillLevels[b] - skillLevels[a])
+  console.log("Sorted skills:", allSkills);
 
   let yPosition = barSpacing; // Initial y-position
 
@@ -354,6 +380,7 @@ async function createSkillBarGraph(skillLevels) {
   allSkills.forEach(skillType => {
     // Get skill level
     const level = skillLevels[skillType];
+    console.log("Skill:", skillType, "Level:", level);
 
     // Calculate width of the bar based on percentage level
     const barWidth = (level / 100) * maxBarWidth;
@@ -364,7 +391,7 @@ async function createSkillBarGraph(skillLevels) {
     bar.setAttribute('y', yPosition);
     bar.setAttribute('width', barWidth);
     bar.setAttribute('height', barHeight);
-    bar.setAttribute('fill', 'skyblue'); // Changed bar color
+    bar.setAttribute('fill', 'steelblue'); // Changed bar color
     svg.appendChild(bar);
 
     // Display skill type label inside the bar
